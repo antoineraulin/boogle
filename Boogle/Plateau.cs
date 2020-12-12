baf2b9e281d26fe9aab4219946f9b354f27111cc
Erwan <erwan.regnault@edu.devinci.fr>
2020-12-12 16:04:36 +0100

    changement clase plateau

--- Boogle/Plateau.cs
+++ Boogle/Plateau.cs
@@ -25,16 +25,18 @@ namespace Boogle
         public Plateau(De[] _board)
         {
             Random r = new Random();
-
+            //On fait les verif pour savoir si notre entrée correspond à ceux qu'on veut.
             if (_board != null)
             {
                 if (_board.Length != 0)
                 {
                     if (_board.Length == 16)
                     {
+                        //Si les tailles sont bonnes alors on envoie.
                         board = _board;
                         for (int i = 0; i < board.Length; i++)
                         {
+                            //On envoie les faces superieures.
                             upperValue[i] = board[i].Roll(r);
                         }
                     }
@@ -43,16 +45,23 @@ namespace Boogle
 
 
         }
+        /// <summary>
+        /// Description du plateau.
+        /// </summary>
+        /// <returns>variable de type string affichant le plateaude jeu.</returns>
         public override string ToString()
         {
 
             string resul = "";
+            //On passe à travers tout le tableau.
             for (int i = 0; i < 16; i++)
             {
-
+                //On entre les valeurs dans un string.
                 resul += $"{upperValue[i]}" + " ";
+                //Si c'est un multiple de 4 alors :
                 if (i % 4 == 0)
                 {
+                    //On mets à la ligne.
                     resul += "\n";
                 }
 
@@ -60,6 +69,10 @@ namespace Boogle
             return resul;
 
 
+        }
+        public bool Test_Plateau(string mot) 
+        {
+            
         }
     }
 }
